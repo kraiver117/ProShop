a9d1a2a27b34364352972f2ffc499f153648862d
Angel <gutierrezramosjoseangel@gmail.com>
2020-10-11 13:31:46 -0500

    Add to cart functionality and Cart Screen added

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -9,7 +9,7 @@ import Loader from '../components/Loader'
 import Message from '../components/Message'
 
 const ProductScreen = ({ history, match: { params } }) => {
-    const [qty, setQty] = useState(0);
+    const [qty, setQty] = useState(1);
 
     const dispatch = useDispatch()
 
