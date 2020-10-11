d1bb707b1d5566b9dccf01e0b1b28c753cb14530
Angel <gutierrezramosjoseangel@gmail.com>
2020-10-11 17:33:50 -0500

    User Registration & Password Encryption

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -3,7 +3,7 @@ import generateToken from '../utils/generateToken.js'
 import User from '../models/userModel.js'
 
 // @desc    Auth user & get token
-//@route    post /api/users/login
+//@route    POST /api/users/login
 //@access   Public
 const authUser = asyncHandler(async(req, res) => {
     const { email, password } = req.body
@@ -23,6 +23,41 @@ const authUser = asyncHandler(async(req, res) => {
         throw new Error('Invalid email or password')
     }
 })
+
+
+// @desc    Register a new user
+//@route    POST /api/users
+//@access   Public
+const registerUser = asyncHandler(async(req, res) => {
+    const { name, email, password } = req.body
+
+    const userExist = await User.findOne({ email })
+
+    if(userExist) {
+        res.status(400)
+        throw new Error('User Already exists')
+    }
+
+    const user =await User.create({
+        name,
+        email,
+        password
+    })
+
+    if(user) {
+        res.status(201).json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            isAdmin: user.isAdmin,
+            token: generateToken(user._id)
+        })
+    }else {
+        res.status(400)
+        throw new  Error('Invalid user data')
+    }
+})
+
 
 // @desc    Get user profile
 //@route    GET /api/users/profile
@@ -46,5 +81,6 @@ const getUserProfile = asyncHandler(async(req, res) => {
 
 export {
     authUser,
-    getUserProfile
+    getUserProfile,
+    registerUser
 }
