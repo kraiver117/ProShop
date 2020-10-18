be7aceaa002c8f5f1d90d0b810d2b2801ac6a7c7
Angel <gutierrezramosjoseangel@gmail.com>
2020-10-17 19:09:49 -0500

    Show user in navbar & logout and User register, reducer & Screen added

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,10 +1,22 @@
-import React from 'react';
+import React from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 import { LinkContainer } from 'react-router-bootstrap'
+import { logout } from '../actions/userActions'
 import {
-    Navbar, Nav, Container
-} from 'react-bootstrap';
+    Navbar, Nav, Container, NavDropdown
+} from 'react-bootstrap'
 
 const Header = () => {
+    const dispatch = useDispatch()
+
+    const userLogin = useSelector(state => state.userLogin)
+    const { userInfo } = userLogin
+
+    const logoutHandler = () => {
+        dispatch(logout())
+    }
+
+
     return (
         <header>
             <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
@@ -16,11 +28,36 @@ const Header = () => {
                     <Navbar.Collapse id="basic-navbar-nav">
                         <Nav className="ml-auto">
                             <LinkContainer to='/cart'>
-                                <Nav.Link> <i className='fas fa-shopping-cart'>Cart</i></Nav.Link>
+                                <Nav.Link>
+                                    <i className='fas fa-shopping-cart'>
+                                        Cart
+                                    </i>
+                                </Nav.Link>
                             </LinkContainer>
-                            <LinkContainer to='/login'>
-                                <Nav.Link> <i className='fa fa-user'>Sign In</i></Nav.Link>
-                            </LinkContainer>
+                            {
+                                userInfo
+                                    ? (
+                                        <NavDropdown title={userInfo.name} id='username'>
+                                            <LinkContainer to='/profile'>
+                                                <NavDropdown.Item>
+                                                    Profile
+                                                </NavDropdown.Item>
+                                            </LinkContainer>
+                                            <NavDropdown.Item onClick={logoutHandler}>
+                                                    Logout
+                                            </NavDropdown.Item>
+                                        </NavDropdown>
+                                    )
+                                    :
+                                    <LinkContainer to='/login'>
+                                        <Nav.Link>
+                                            <i className='fa fa-user'>
+                                                Sign In
+                                            </i>
+                                        </Nav.Link>
+                                    </LinkContainer>
+                            }
+
                         </Nav>
                     </Navbar.Collapse>
                 </Container>
--- frontend/src/reducers/userReducers.js
+++ frontend/src/reducers/userReducers.js
@@ -1,19 +1,20 @@
-import { 
-    USER_LOGIN_FAIL, 
-    USER_LOGIN_REQUEST, 
-    USER_LOGIN_SUCCESS, 
-    USER_LOGOUT
+import {
+    USER_LOGIN_FAIL,
+    USER_LOGIN_REQUEST,
+    USER_LOGIN_SUCCESS,
+    USER_LOGOUT,
+    USER_REGISTER_FAIL,
+    USER_REGISTER_SUCCESS,
+    USER_REGISTER_REQUEST
 } from "../constants/userConstants"
 
-
-
-export const userLoginReducer = (state = { }, action) => {
+export const userLoginReducer = (state = {}, action) => {
     switch (action.type) {
         case USER_LOGIN_REQUEST:
             return {
-                loading:true
+                loading: true
             }
-        
+
         case USER_LOGIN_SUCCESS:
             return {
                 loading: false,
@@ -25,12 +26,35 @@ export const userLoginReducer = (state = { }, action) => {
                 loading: false,
                 error: action.payload
             }
-        
+
         case USER_LOGOUT:
-            return { }
+            return {}
 
         default:
             return state
+    }
+}
 
+export const userRegisterReducer = (state = {}, action) => {
+    switch (action.type) {
+        case USER_REGISTER_REQUEST:
+            return {
+                loading: true
+            }
+
+        case USER_REGISTER_SUCCESS:
+            return {
+                loading: false,
+                userInfo: action.payload
+            }
+
+        case USER_REGISTER_FAIL:
+            return {
+                loading: false,
+                error: action.payload
+            }
+
+        default:
+            return state
     }
 }
