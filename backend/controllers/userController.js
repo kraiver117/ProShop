8f114f4b3ebcbf27a250ac383a4d8b2c24a19544
Angel <gutierrezramosjoseangel@gmail.com>
2020-10-30 18:16:29 -0600

    Admin Middleware & Get Users Endpoint

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -108,9 +108,19 @@ const updateUserProfile = asyncHandler(async(req, res) => {
     }
  })
 
+// @desc    Get alll users
+//@route    GET /api/users/
+//@access   Private/Admin
+const getUsers = asyncHandler(async(req, res) => {
+    const users = await User.find({})
+
+    res.json(users)   
+ })
+
 export {
     authUser,
     getUserProfile,
     registerUser,
-    updateUserProfile
+    updateUserProfile,
+    getUsers
 }
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,12 +1,11 @@
 import express from 'express'
-import { authUser, getUserProfile, registerUser, updateUserProfile } from '../controllers/userController.js'
-import { protect } from '../middleware/authMiddleware.js'
+import { authUser, getUserProfile, getUsers, registerUser, updateUserProfile } from '../controllers/userController.js'
+import { protect, admin } from '../middleware/authMiddleware.js'
 
 const router = express.Router()
 
-router.route('/').post(registerUser)
+router.route('/').post(registerUser).get(protect, admin, getUsers)
 router.post('/login', authUser)
 router.route('/profile').get(protect, getUserProfile).put(protect, updateUserProfile)
-
 
 export default router
