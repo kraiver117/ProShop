df7256dda6b7ae7020ce9d8a80529b2aa0bf2f84
Angel <gutierrezramosjoseangel@gmail.com>
2020-11-14 01:27:57 -0600

    Admin Delete Products

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -24,7 +24,23 @@ const getProductById = asyncHandler(async(req, res) => {
     }
 })
 
+// @desc    Delete a product
+//@route    DELETE /api/products/:id
+//@access   Private/Admin
+const deleteProduct = asyncHandler(async(req, res) => {
+    const product = await Product.findById(req.params.id)
+
+    if (product) {
+        await product.remove()
+        res.json({ message: 'Product removed' })
+    } else {
+        res.status(404)
+        throw new Error('Product not Found')
+    }
+})
+
 export {
     getProductById,
-    getProducts
+    getProducts,
+    deleteProduct
 }
--- frontend/src/reducers/productReducers.js
+++ frontend/src/reducers/productReducers.js
@@ -1,20 +1,23 @@
-import { 
-    PRODUCT_LIST_REQUEST, 
+import {
+    PRODUCT_LIST_REQUEST,
     PRODUCT_LIST_SUCCESS,
     PRODUCT_LIST_FAIL,
     PRODUCT_DETAILS_REQUEST,
     PRODUCT_DETAILS_SUCCESS,
     PRODUCT_DETAILS_FAIL,
+    PRODUCT_DELETE_REQUEST,
+    PRODUCT_DELETE_SUCCESS,
+    PRODUCT_DELETE_FAIL,
 } from '../constants/productConstants'
 
-export const productListReducer = (state = {products: [] }, action) => {
+export const productListReducer = (state = { products: [] }, action) => {
     switch (action.type) {
         case PRODUCT_LIST_REQUEST:
             return {
-                loading:true,
+                loading: true,
                 products: []
             }
-        
+
         case PRODUCT_LIST_SUCCESS:
             return {
                 loading: false,
@@ -33,14 +36,14 @@ export const productListReducer = (state = {products: [] }, action) => {
     }
 }
 
-export const productDetailsReducer = (state = {product: { reviews: [] } }, action) => {
+export const productDetailsReducer = (state = { product: { reviews: [] } }, action) => {
     switch (action.type) {
         case PRODUCT_DETAILS_REQUEST:
             return {
-                loading:true,
+                loading: true,
                 ...state
             }
-        
+
         case PRODUCT_DETAILS_SUCCESS:
             return {
                 loading: false,
@@ -48,6 +51,31 @@ export const productDetailsReducer = (state = {product: { reviews: [] } }, actio
             }
 
         case PRODUCT_DETAILS_FAIL:
+            return {
+                loading: false,
+                error: action.payload
+            }
+
+        default:
+            return state
+
+    }
+}
+
+export const productDeleteReducer = (state = {}, action) => {
+    switch (action.type) {
+        case PRODUCT_DELETE_REQUEST:
+            return {
+                loading: true
+            }
+
+        case PRODUCT_DELETE_SUCCESS:
+            return {
+                loading: false,
+                success: true
+            }
+
+        case PRODUCT_DELETE_FAIL:
             return {
                 loading: false,
                 error: action.payload
