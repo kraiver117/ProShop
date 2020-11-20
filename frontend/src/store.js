d2619fb476d00c3047db922cc5d6d01503d51722
Angel <gutierrezramosjoseangel@gmail.com>
2020-11-20 15:39:44 -0600

    Front End Product Reviews

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -5,7 +5,8 @@ import { productListReducer,
     productDetailsReducer, 
     productDeleteReducer,
     productCreateReducer,
-    productUpdateReducer
+    productUpdateReducer,
+    productReviewCreateReducer
 } from './reducers/productReducers'
 import { cartReducer } from './reducers/cartReducers'
 import { 
@@ -32,6 +33,7 @@ const reducer = combineReducers({
     productDelete: productDeleteReducer,
     productCreate: productCreateReducer,
     productUpdate: productUpdateReducer,
+    productReviewCreate: productReviewCreateReducer,
     cart: cartReducer,
     userLogin: userLoginReducer,
     userRegister: userRegisterReducer,
